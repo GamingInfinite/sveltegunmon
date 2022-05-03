957f13de54001a25e017e49888e8b28f357b0a04
Snowpack Build <actions@github.com>
2022-05-03 17:09:24 +0000

    Commit from GitHub Actions (Snowpack)

--- docs/dist/Components/NavButton.svelte.js
+++ docs/dist/Components/NavButton.svelte.js
@@ -77,6 +77,7 @@ function instance($$self, $$props, $$invalidate) {
 	function handleclick() {
 		var modal = document.getElementById(modal_id);
 		modal.style.display = "block";
+		modal.style.opacity = 1;
 	}
 
 	$$self.$$set = $$props => {
